4ee3a56046876746d4e3690c33e4b0e94b9b3bb0
overloper <overloper0225@gmail.com>
2024-07-18 15:01:30 +0900

    feat: add structs of elements needed for the game

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+// player 구조체
+struct player {
+	// 경험치, 체력, 레벨, 행운력, 공격력, 들고있는 아이템(문자열), 아이템들(문자열 배열)
+	int level;
+	int hp;
+	double attack;
+	double luck;
+	double exp;
+	char* item;
+	char* bags;
+};
+
+// 아이템 구조체
+struct armor_item {
+	// 체력 증가, 행운력 증가
+	int increase_hp;
+	double increase_luck;
+};
+
+struct attack_item {
+	// 공격력증가
+	double increase_attack;
+};
+
+// 적 구조체
+struct enemy {
+	// 체력, 레벨, 공격력, 몹이 들고있는 아이템(문자열), 몹 유형(졸개, 보스)
+	int hp;
+	int level;
+	int attack;
+	char* mop_item;
+	char* mop_category;
+};
+
+int main(void) {
+	
+	return 0;
+}
