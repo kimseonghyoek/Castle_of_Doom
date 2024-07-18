ecd1111e0a15f84484ec0dc3475c78168223812e
overloper <overloper0225@gmail.com>
2024-07-18 16:20:14 +0900

    feat: add a lots of functions

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // player 구조체
 struct player {
 	// 경험치, 체력, 레벨, 행운력, 공격력, 들고있는 아이템(문자열), 아이템들(문자열 배열)
 	int level;
 	int hp;
-	double attack;
-	double luck;
+	float attack;
+	float luck;
 	double exp;
 	char* item;
 	char* bags;
@@ -29,17 +30,29 @@ struct enemy {
 	// 체력, 레벨, 공격력, 몹이 들고있는 아이템(문자열), 몹 유형(졸개, 보스)
 	int hp;
 	int level;
-	int attack;
+	float attack;
 	char* mop_item;
 	char* mop_category;
 };
 
+// 함수 선언부
 void showLayout();
+void runAway();
+int attack(float damege, int hp);
+char* selectItem(char *itmes);
+
 
 int main(void) {
-	showLayout();
+	//showLayout();
+	runAway();
+	
+	
+	
+	
 	return 0;
 }
+
+
 
 // 게임 실행시 보여줄 레이아웃
 void showLayout() {
@@ -76,3 +89,27 @@ void showLayout() {
 	printf("│                                                            │                    │\n");
 	printf("└────────────────────────────────────────────────────────────┘────────────────────┘\n");
 };
+
+// 전투시 도망(0~9중에서 6까지는 도망이 가능하고 6보다 크면 도망에 못가는 확률)
+void runAway() {
+	srand(time(NULL));
+	int random = 0;
+	random = rand() % 9;
+	printf("%d\n", random);
+	if (random <= 6) {
+		printf("무사히 도망쳤습니다.");
+	}
+	else {
+		printf("도망가지 못하였습니다.");
+	}
+};
+
+// 전투시 공격(parameter로 현재 공격력과 공격받는 상대의 체력을 받음. 후에 공격을 받은 후의 체력을 반환함.)
+int attack(float damege, int hp) {
+	return 0;
+}
+
+// 현재 가지고 있는 아이템 중 선택
+char* selectItems(char* items) {
+	return "items";
+}
